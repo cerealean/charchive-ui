3b7e3a67c4c89c1f45246879f1befe8295a1abdb
Michael Crawford <cerealean@users.noreply.github.com>
2026-06-06 18:22:48 -0500

    Add landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./app').then((m) => m.App),
+    loadComponent: () => import('./landing/landing').then((m) => m.Landing),
   },
   {
     path: 'login',
