b733deaae4a218b7090d6f4851ad7e45d8b9c968
Michael Crawford <cerealean@users.noreply.github.com>
2026-06-06 17:36:54 -0500

    Change local development target

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://nrmbxhtklwgzvwhjreki.supabase.co',
-  supabasePublishableKey: 'sb_publishable_izXphjYrEzNZcTmCt0jRCw_u__afYuE',
+  supabaseUrl: 'http://127.0.0.1:54321',
+  supabasePublishableKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRFA0NiK7kyqd6Is3hbHH9Kb8FSf6L5CweMvEE9eiSk',
 };
